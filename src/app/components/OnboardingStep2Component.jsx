8703e43d421576db5a16559b470db1f4dc6794c3
Appleeyes <appleeyespackage@gmail.com>
2024-04-08 01:23:58 +0100

    fix: modified onboarding components 2

--- src/app/components/OnboardingStep2Component.jsx
+++ src/app/components/OnboardingStep2Component.jsx
@@ -5,12 +5,14 @@ import Button from "../components/ButtonComponent";
 function OnboardingStep2() {
   return (
     <div className='onboard2'>
-      <div className="background-image"></div>
-      <div className="text-container">
-        <h5>Get Loans</h5>
-        <p>Get loans for your accoundation</p>
-        <Button link="#">Next</Button>
+      <div className="background-image">
+        <div className="text-container">
+          <h5>Get Loans</h5>
+          <p>Get loans for your accoundation</p>
+          <Button link="#">Next</Button>
+        </div>
       </div>
+
     </div>
   );
 }
--- src/app/components/OnboardingStep3Component.jsx
+++ src/app/components/OnboardingStep3Component.jsx
@@ -5,11 +5,12 @@ import Button from "../components/ButtonComponent";
 function OnboardingStep3() {
   return (
     <div className='onboard3'>
-      <div className="background-image"></div>
+      <div className="background-image">
       <div className="text-container">
         <h5>Flexible Payment</h5>
         <p>Low interest and flexible payment options</p>
         <Button link="#/plans">Next</Button>
+      </div>
       </div>
     </div>
   );
